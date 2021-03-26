2a35a8fad695851c9e57caf8cc215f4c7afdb8e6
unknown <gkpippal@gmail.com>
2021-03-26 13:21:00 +0530

    task 1 completed :: gagan kumar pippal::

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import{FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'feedbackForm';
+  feedbackForm: FormGroup;
+
+  ngOnInit(){
+    this.feedbackForm =new FormGroup({
+      
+        'username': new FormControl(null,[Validators.required]),
+        'hoursSpent': new FormControl(null,[Validators.required]),
+        'ageGroup': new FormControl(null,[Validators.required]),
+        'age': new FormControl(null,[Validators.required]),
+        'movies':new FormControl(null,[Validators.required]),
+     
+    });
+  }
+  
+  onSubmit(){
+    console.log(this.feedbackForm)
+    console.log(this.feedbackForm.value.movies.split(","))
+  }
+
+  checkHoursSpent(){
+    const hoursSpent = this.feedbackForm.value.hoursSpent;
+    if (hoursSpent>168){
+      this.feedbackForm.patchValue({'hoursSpent':null})
+    }
+  }
+
+  titledUserName(){
+    const userName = this.feedbackForm.value.username
+    const titleCase = userName.charAt(0).toUpperCase()+ userName.slice(1);
+    this.feedbackForm.patchValue({'username': titleCase})
+  }
+
+  onAgeGroupSelect(){
+    const ageGroup= this.feedbackForm.value.ageGroup;
+    const age =this.feedbackForm.value.age;
+    // console.log(ageGroup)
+    if(Number(ageGroup)=== 0){
+      if(0>age && age<13){
+        this.feedbackForm.patchValue({'age':null})
+      }
+    }
+    else if (Number(ageGroup)===1){
+      if(age>19 && age<13){
+        this.feedbackForm.patchValue({'age': null})
+      }
+    }
+    else if(Number(ageGroup)===2){
+      if(age<55){
+        this.feedbackForm.patchValue({'age':null})
+      }
+    }
+    else if(Number(ageGroup)===3){
+      if (age>55){
+        this.feedbackForm.patchValue({'age':null})
+      }
+    }
+  }
 }
+
+
